fb1ecd78635ff9289f4c9940d6a5f1c77138f86c
shakev1 <shalevshemesh@gmail.com>
2025-01-30 09:21:41 +0200

    change to game

--- backend/models/Game.py
+++ backend/models/Game.py
@@ -3,7 +3,7 @@
 from . import db
 
 
-class Book(db.Model):
+class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     author = db.Column(db.String(200), nullable=False)
--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,3 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()  # this Creates the database instance and 
+db = SQLAlchemy()
